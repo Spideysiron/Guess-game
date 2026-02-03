2456086944b5375e643d77013ce12093ec6da67d
Himanshu <ur.siron07@gmail.com>
2026-02-03 11:59:03 +0530

    version 1

---
+++ calculator.py
@@ -0,0 +1,24 @@
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+oper = input("Choose operation (+,-,*,/): ").strip()
+
+ 
+ #addition
+if ( oper=="+" ):
+    print(f"Addition : {num1 + num2}")
+
+elif ( oper=="-" ):
+    print(f"Substraction : {num1 - num2}")
+
+elif ( oper=="*" ):
+    print(f"Multiplication : {num1*num2}")
+
+elif ( oper=="/" ):
+    if num2==0:
+        print("Cannot divide by zero!")
+
+    else:
+        print(f"Division : {num1/num2}")
+
+else:
+    print("enter a valid operation")
